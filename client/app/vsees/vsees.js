91d0fb9671c66a178065ac28ccbc98c757821e29
swzhou <swing.zhou@hotmail.com>
2014-09-07 22:12:35 +0800

    [sw] authenticate check for updating VSees

--- client/app/vsees/vsees.js
+++ client/app/vsees/vsees.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('vSeeUApp')
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/vsees', {
-        templateUrl: 'app/vsees/vsees.html',
-        controller: 'VseesCtrl'
-      });
-  });
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/vsees', {
+                templateUrl: 'app/vsees/vsees.html',
+                controller: 'VseesCtrl',
+                authenticate: true
+            });
+    });

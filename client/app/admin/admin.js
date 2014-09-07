57d4d0a738fc937786e9cbcbf917a5360244493b
swzhou <swing.zhou@hotmail.com>
2014-09-07 09:38:09 +0800

    [sw] initialize project from yoeman template - angular-fullstack

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('vSeeUApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });

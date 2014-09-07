13d9d9f231706185cf839ad331118dc40085ffcc
swzhou <swing.zhou@hotmail.com>
2014-09-07 22:35:16 +0800

    [sw] append current user as owner when creating new vsee

--- client/app/vsees/vsees.controller.js
+++ client/app/vsees/vsees.controller.js
@@ -1,33 +1,37 @@
 'use strict';
 
 angular.module('vSeeUApp')
-    .controller('VseesCtrl', ['$scope', '$http', 'socket', function ($scope, $http, socket) {
-        $scope.vsees = [];
-        $scope.newVsee = {};
+    .controller('VseesCtrl', ['$scope', '$http', 'socket', 'User',
+        function ($scope, $http, socket, User) {
+            $scope.vsees = [];
+            $scope.newVsee = {};
 
-        $http.get('/api/vsees').success(function (vsees) {
-            $scope.vsees = vsees;
-            socket.syncUpdates('vsee', $scope.vsees);
-        });
-
-        $scope.save = function (vsee) {
-            $http.put('/api/vsees/' + vsee._id, vsee);
-        };
-
-        $scope.remove = function (vsee) {
-            $http.delete('/api/vsees/' + vsee._id);
-        };
-
-        $scope.add = function() {
-            if (_.isEmpty($scope.newVsee)) {
-                return;
-            }
-            $http.post('/api/vsees', $scope.newVsee).then(function() {
-                $scope.newVsee = {};
+            $http.get('/api/vsees').success(function (vsees) {
+                $scope.vsees = vsees;
+                socket.syncUpdates('vsee', $scope.vsees);
             });
-        };
 
-        $scope.$on('$destroy', function () {
-            socket.unsyncUpdates('vsee');
-        });
-    }]);
+            $scope.save = function (vsee) {
+                $http.put('/api/vsees/' + vsee._id, vsee);
+            };
+
+            $scope.remove = function (vsee) {
+                $http.delete('/api/vsees/' + vsee._id);
+            };
+
+            $scope.add = function () {
+                if (_.isEmpty($scope.newVsee)) {
+                    return;
+                }
+                User.get().$promise.then(function (currentUser) {
+                    var newVsee = _.extend($scope.newVsee, {owner: currentUser._id});
+                    $http.post('/api/vsees', newVsee).then(function () {
+                        $scope.newVsee = {};
+                    });
+                });
+            };
+
+            $scope.$on('$destroy', function () {
+                socket.unsyncUpdates('vsee');
+            });
+        }]);
